38982129840f54dae5dddc43d447844b6f00d6c0
Herbert Lange <github@pietaetskirsche.de>
2023-12-01 16:29:55 +0100

    new attempt for a publishing function

--- .github/workflows/publish.js
+++ .github/workflows/publish.js
@@ -1,13 +1,15 @@
 const moment = require('moment')
-const tag_name = moment().format('YYYYMMDD-HHmmss')
-// const releaseInfo = github.rest.repos.createRelease({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     tag_name})
-// //            github.rest.repos.uploadReleaseAsset( {
-// // await
-// github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     release_id: releaseInfo.id,
-//     data: '@file.lst'})
+function publish(github, context) {
+	tag_name = moment().format('YYYYMMDD-HHmmss');
+	releaseInfo = github.rest.repos.createRelease({
+	     owner: context.repo.owner,
+	     repo: context.repo.repo,
+	     tag_name});
+	github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
+	     owner: context.repo.owner,
+	     repo: context.repo.repo,
+	     release_id: releaseInfo.id,
+	     data: '@file.lst'});
+    }
+
+exports.publish=publish;
