727b5e7637b04f8947d389c988b2a26d70eea70e
Herbert Lange <github@pietaetskirsche.de>
2023-12-01 15:34:25 +0100

    comment out github code

--- .github/workflows/publish.js
+++ .github/workflows/publish.js
@@ -1,13 +1,13 @@
 const moment = require('moment')
 const tag_name = moment().format('YYYYMMDD-HHmmss')
-const releaseInfo = github.rest.repos.createRelease({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    tag_name})
-//            github.rest.repos.uploadReleaseAsset( {
-// await
-github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    release_id: releaseInfo.id,
-    data: '@file.lst'})
+// const releaseInfo = github.rest.repos.createRelease({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     tag_name})
+// //            github.rest.repos.uploadReleaseAsset( {
+// // await
+// github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     release_id: releaseInfo.id,
+//     data: '@file.lst'})
