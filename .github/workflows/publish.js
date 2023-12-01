bbe568f1cb2b2471e2600bf0a731bbd0adde0843
Herbert Lange <github@pietaetskirsche.de>
2023-12-01 15:21:32 +0100

    remove await keyword

--- .github/workflows/publish.js
+++ .github/workflows/publish.js
@@ -5,7 +5,8 @@ const releaseInfo = github.rest.repos.createRelease({
     repo: context.repo.repo,
     tag_name})
 //            github.rest.repos.uploadReleaseAsset( {
-await github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
+// await
+github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
     owner: context.repo.owner,
     repo: context.repo.repo,
     release_id: releaseInfo.id,
