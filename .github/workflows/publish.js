f02c7938218ee8a716f95b8ac6232c87f0c24264
Herbert Lange <github@pietaetskirsche.de>
2023-12-06 17:15:15 +0100

    next attempt

--- .github/workflows/publish.js
+++ .github/workflows/publish.js
@@ -6,13 +6,31 @@ function publish(github, context) {
 	     repo: context.repo.repo,
 	     tag_name});
         console.log(releaseInfo);
-        github.rest.repos.uploadReleaseAsset({
-	     owner: context.repo.owner,
-	     repo: context.repo.repo,
-	     release_id: tag_name,
-	     name: "test.txt",
-	    data: "testdata",
-});
+//         github.rest.repos.uploadReleaseAsset({
+// 	     owner: context.repo.owner,
+// 	     repo: context.repo.repo,
+// 	     release_id: tag_name,
+// 	     name: "test.txt",
+// 	    data: "testdata",
+    // });
+        release = octokit.rest.repos.getReleaseByTag({
+             owner: context.repo.owner,
+             repo: context.repo.repo,
+             tag: tag_name,
+	});
+        releaseId = release.data.id;
+        releaseUrl = release.data.upload_url;
+        octokit.request({
+          method: "POST",
+          url: releaseUrl,
+          headers: {
+            "content-type": "text/plain",
+          },
+          data: "Hello, world!\n",
+          name: "test-upload.txt",
+          label: "test",
+        });
+      })
 //	github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
 //	     owner: context.repo.owner,
 //	     repo: context.repo.repo,
